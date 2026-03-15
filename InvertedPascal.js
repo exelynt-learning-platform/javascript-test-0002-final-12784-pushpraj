b942659d879b2df0bd8dd3d4a80e8ecdff03ef87
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 21:00:23 +0530

    Update and rename PalindromePattern.js to InvertedPascal.js

---
+++ InvertedPascal.js
@@ -0,0 +1,16 @@
+const totalRows = 5;
+
+// We start from n = 4 (the 5th row of Pascal's triangle) and go down to 0
+for (let n = totalRows - 1; n >= 0; n--) {
+    let rowStr = " ".repeat((totalRows - 1 - n)); // Add leading alignment spaces
+    let val = 1;
+
+    for (let k = 0; k <= n; k++) {
+        rowStr += val + " ";
+        
+        // Efficiently calculate the next binomial coefficient:
+        val = val * (n - k) / (k + 1);
+    }
+    
+    console.log(rowStr);
+}
