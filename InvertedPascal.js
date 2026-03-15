74f63598c73c4a5d8e51ac2b1b2ea7e6ac9fa6e6
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 21:06:09 +0530

    Update InvertedPascal.js

--- InvertedPascal.js
+++ InvertedPascal.js
@@ -1,4 +1,4 @@
-const totalRows = 5;
+const totalRows = 5; // Total rows in our inverted pascal triangle
 
 // We start from n = 4 (the 5th row of Pascal's triangle) and go down to 0
 for (let n = totalRows - 1; n >= 0; n--) {
